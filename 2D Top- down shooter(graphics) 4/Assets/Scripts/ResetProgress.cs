9adb00c6e8553594834ae0622880228114933039
SantJagoBro <109796823+SaintJago@users.noreply.github.com>
2024-12-15 17:03:54 +0200

    Bug fix

--- 2D Top- down shooter(graphics) 4/Assets/Scripts/ResetProgress.cs
+++ 2D Top- down shooter(graphics) 4/Assets/Scripts/ResetProgress.cs
@@ -5,32 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class ResetProgress : MonoBehaviour 
 {
-    public void ResetAllProgress() 
+    public void ResetAllProgress()
     {
-        // Очистка PlayerPrefs 
-        PlayerPrefs.DeleteAll(); 
-        PlayerPrefs.Save();  
-
-        // Очистка BayatGames сохранений 
-        SaveGame.DeleteAll(); 
-        Debug.Log("All progress reset");  
-
-        // Запуск корутины с задержкой перезагрузки сцены 
-        StartCoroutine(ReloadSceneWithDelay()); 
-    }  
-
-    private IEnumerator ReloadSceneWithDelay() 
-    {
-        // Ожидание 1 секунды перед перезагрузкой 
-        yield return new WaitForSeconds(1f);  
-
-        // Асинхронная загрузка сцены
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
-
-        // Ожидаем полной загрузки сцены
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        SaveGame.DeleteAll();
+        
+        Debug.Log("All progress reset");
+        
+        // Immediate scene reload
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
